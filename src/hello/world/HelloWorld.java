00b3d168f568686ebf16582b52fe9ed565ffaaa1
John Stringer <johnstringer@cdnis.edu.hk>
2020-02-10 13:05:09 +0800

    Hello to Infinity

--- src/hello/world/HelloWorld.java
+++ src/hello/world/HelloWorld.java
@@ -17,7 +17,7 @@ public class HelloWorld {
      */
         
     public static void main(String[] args) {
-       System.out.println("Hello Universe");
+       System.out.println("Hello to Infinity & Beyond");
         
     }
     
